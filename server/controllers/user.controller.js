289548777f83f5ab689c99fa8a6a6f613f6925dc
nnayaww <nnayaww16@gmail.com>
2024-06-14 20:57:56 +0000

    chore: Update updateUserController to handle user updates and improve error handling

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,24 @@
 import { updateUser } from "../services/user.service.js";
 
 export const updateUserController = async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            if (req.body.password) {
+                req.body.password = bcrypt.hashSync(req.body.password, 10);
+            }
+            const updatedUser = await updateUser(req.params.id, req.body);
+            res.status(200).json({
+                updatedUser,
+                message: "User has been updated",
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                error: error,
+                message: "User can only be updated",
+            });
+        }
+
     try {
         const updatedUser = await updateUser(req.params.id, req.body);
         res.status(200).json({
@@ -15,3 +33,4 @@ export const updateUserController = async (req, res) => {
         });
     }
 }
+};
