8e61a51345f67b0de9a4d1911cf5f85c0151835c
nnayaww <nnayaww16@gmail.com>
2024-06-15 11:28:52 +0000

    firebase

---
+++ server/controllers/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHIMRTQJ-YWvoyjs78VHTDmClYyFeu_7Y",
+  authDomain: "playlix-a21e3.firebaseapp.com",
+  projectId: "playlix-a21e3",
+  storageBucket: "playlix-a21e3.appspot.com",
+  messagingSenderId: "1034108951137",
+  appId: "1:1034108951137:web:924a0da96f1b63b5c42820"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
